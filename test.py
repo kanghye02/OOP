355ff93f70085c285e70932d2ef2a464ed13eb60
kanghye02 <115169462+kanghye02@users.noreply.github.com>
2024-02-03 10:47:57 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-db = mysql.connector.connect(user = 'root',password='root', host='localhost',database='TEST1')
-code ="DROP DATABASE `test` ;"
-
-mycursor = db.cursor()
-mycursor.execute("")
